714c5b139072c0871f7cf1b3903d35b3d71dfb3f
ubergesundheit <ubergesundheit@googlemail.com>
2014-03-24 11:30:41 +0100

    add logitech mouse to mousespeed.sh

--- mousespeed.sh
+++ mousespeed.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 xinput set-prop 'Razer Razer DeathAdder' 'Device Accel Profile' -1
-#xinput set-prop 'Razer Razer Diamondback Optical Mouse' 'Device Accel Profile' -1
+xinput set-prop 'Logitech USB Optical Mouse' 'Device Accel Profile' -1
